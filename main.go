c0f8aacad53e833b4bb344e6043a44acf66b0cda
Joseph King <joe.k659@gmail.com>
2023-06-12 20:35:32 -0500

    second commit

--- main.go
+++ main.go
@@ -1,7 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+import (
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello world")
+
+	godotenv.Load(".env")
+	portString := os.Getenv("PORT")
+
+	if portString == "" {
+		log.Fatal("Port is not found in the environment")
+	}
+
+	fmt.Println("Port: ", portString)
+
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	v1router := chi.NewRouter()
+
+	v1router.Get("/healthz", handlerReadiness)
+	v1router.Get("/err", handlerErr)
+
+	router.Mount("/v1", v1router)
+
+	serve := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	log.Printf("Server starting on port %v", portString)
+	err := serve.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
